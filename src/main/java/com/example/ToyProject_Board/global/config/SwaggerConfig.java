38fbc7bf1cc6a5bacc99d1b8c36251dabad1fc8f
Erichong7 <eirchong7@gmail.com>
2026-07-10 05:04:56 +0900

    docs: Add title and description to Swagger OpenAPI info

--- src/main/java/com/example/ToyProject_Board/global/config/SwaggerConfig.java
+++ src/main/java/com/example/ToyProject_Board/global/config/SwaggerConfig.java
@@ -2,6 +2,7 @@ package com.example.ToyProject_Board.global.config;
 
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
@@ -15,6 +16,9 @@ public class SwaggerConfig {
     @Bean
     public OpenAPI openAPI() {
         return new OpenAPI()
+                .info(new Info()
+                        .title("연습 일정 API")
+                        .description("명지대학교 댄스 동아리 MGH의 연습 일정 조절과 아카이빙을 위한 API"))
                 .components(new Components().addSecuritySchemes(BEARER_SCHEME_NAME,
                         new SecurityScheme()
                                 .type(SecurityScheme.Type.HTTP)
